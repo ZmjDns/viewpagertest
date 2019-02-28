c1b8aad3b60794a2a580ef02d6d335bdbb4d9c7e
ZmjDns <292570927@qq.com>
2019-02-28 12:25:46 +0800

    引入下拉刷新上拉加载+基础配置

---
+++ app/src/main/java/com/zmj/viewpagertest/MyApplication.java
@@ -0,0 +1,48 @@
+package com.zmj.viewpagertest;
+
+import android.app.Application;
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+import com.scwang.smartrefresh.layout.api.DefaultRefreshFooterCreator;
+import com.scwang.smartrefresh.layout.api.DefaultRefreshHeaderCreator;
+import com.scwang.smartrefresh.layout.api.RefreshFooter;
+import com.scwang.smartrefresh.layout.api.RefreshHeader;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.footer.ClassicsFooter;
+import com.scwang.smartrefresh.layout.header.ClassicsHeader;
+
+/**
+ * Author : Zmj
+ * Blog : https://blog.csdn.net/Zmj_Dns
+ * DitHub : https://github.com/ZmjDns
+ * Time : 2019/2/27
+ * Description :
+ */
+public class MyApplication extends Application {
+    static {//静态代码块，避免内存泄漏
+        //设置全局的Header构造器
+        SmartRefreshLayout.setDefaultRefreshHeaderCreator(new DefaultRefreshHeaderCreator() {
+            @NonNull
+            @Override
+            public RefreshHeader createRefreshHeader(@NonNull Context context, @NonNull RefreshLayout layout) {
+                layout.setPrimaryColorsId(R.color.colorPrimary,android.R.color.white);//设置全局主题颜色
+                return new ClassicsHeader(context);
+            }
+        });
+        //设置全局Footer构造器
+        SmartRefreshLayout.setDefaultRefreshFooterCreator(new DefaultRefreshFooterCreator() {
+            @NonNull
+            @Override
+            public RefreshFooter createRefreshFooter(@NonNull Context context, @NonNull RefreshLayout layout) {
+                //指定为经典Footer，默认为BallPulseFooter
+                return new ClassicsFooter(context).setDrawableArrowSize(20);
+            }
+        });
+    }
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
