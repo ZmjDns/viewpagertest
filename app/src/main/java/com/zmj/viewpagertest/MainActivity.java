77383f17432ae2ca19f252d1ab84a1125f387c7c
ZmjDns <292570927@qq.com>
2019-03-04 20:49:20 +0800

    引入下拉刷新上拉加载+基础配置

--- app/src/main/java/com/zmj/viewpagertest/MainActivity.java
+++ app/src/main/java/com/zmj/viewpagertest/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.ImageView;
 import com.bumptech.glide.Glide;
 import com.squareup.picasso.Picasso;
 import com.zmj.viewpagertest.horizontalviewpager.TestHorizontalViewPagerOneAct;
+import com.zmj.viewpagertest.recyclerview.TestRecyclerViewOneAct;
 import com.zmj.viewpagertest.verticalviewpager.TestVerticalViewPagerOneAct;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         iv_img = findViewById(R.id.iv_img);
 
         //Glide.with(this).load("http://192.168.1.254:8080/corvertSXWWData/pics/view_6.jpg").placeholder(R.drawable.palceholder).into(iv_img);
-        Picasso.with(this).load("http://192.168.1.254:8080/corvertSXWWData/pics/view_6.jpg").placeholder(R.drawable.palceholder).into(iv_img);
+        //Picasso.with(this).load("http://192.168.1.254:8080/corvertSXWWData/pics/view_6.jpg").placeholder(R.drawable.palceholder).into(iv_img);
     }
 
     public void horizViewPager(View view){
@@ -32,10 +33,13 @@ public class MainActivity extends AppCompatActivity {
     public void vertiaclViewPager(View view){
         startActivity(new Intent(this, TestVerticalViewPagerOneAct.class));
     }
+    public void recyclerView(View view){
+        startActivity(new Intent(this, TestRecyclerViewOneAct.class));
+    }
 
     @Override
     protected void onDestroy() {
-        Glide.clear(iv_img);
+        //Glide.clear(iv_img);
         super.onDestroy();
     }
 }
---
+++ app/src/main/java/com/zmj/viewpagertest/entry/CardInfo.java
@@ -0,0 +1,12 @@
+package com.zmj.viewpagertest.entry;
+
+/**
+ * Author : Zmj
+ * Blog : https://blog.csdn.net/Zmj_Dns
+ * DitHub : https://github.com/ZmjDns
+ * Time : 2019/2/27
+ * Description :
+ */
+public class CardInfo {
+
+}
