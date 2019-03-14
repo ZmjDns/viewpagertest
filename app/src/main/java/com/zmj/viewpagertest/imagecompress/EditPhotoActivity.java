b334797d5d21245ed48ebba38bca0b29b00e7761
ZmjDns <292570927@qq.com>
2019-03-14 23:05:11 +0800

    自定义EditTextView

--- app/src/main/java/com/zmj/viewpagertest/imagecompress/EditPhotoActivity.java
+++ app/src/main/java/com/zmj/viewpagertest/imagecompress/EditPhotoActivity.java
@@ -15,6 +15,7 @@ public class EditPhotoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
     }
 }
