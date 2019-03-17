466d52444990b401d84879f7c4cd5240f60fa4f7
ZmjDns <292570927@qq.com>
2019-03-17 21:48:31 +0800

    子线程中使用Handler

--- app/src/main/java/com/zmj/viewpagertest/imagecompress/EditPhotoActivity.java
+++ app/src/main/java/com/zmj/viewpagertest/imagecompress/EditPhotoActivity.java
@@ -1,8 +1,12 @@
 package com.zmj.viewpagertest.imagecompress;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.HttpAuthHandler;
 
 /**
  * Author : Zmj
@@ -17,5 +21,27 @@ public class EditPhotoActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+    }
+
+
+
+
+    //子线程中使用Handler的方法
+    //https://blog.csdn.net/fnhfire_7030/article/details/79518819
+    class LooperThread extends Thread{
+        public Handler mHandler;
+
+        @Override
+        public void run() {
+            Looper.prepare();
+            mHandler = new Handler(){
+                @Override
+                public void handleMessage(Message msg) {
+                    super.handleMessage(msg);
+                    //
+                }
+            };
+            Looper.loop();
+        }
     }
 }
