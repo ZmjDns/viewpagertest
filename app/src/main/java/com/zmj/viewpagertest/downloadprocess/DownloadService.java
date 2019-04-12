316ccd11bee89fc2ecb0e13dc3148a9870eb785f
ZmjDns <292570927@qq.com>
2019-04-12 22:09:22 +0800

    断点下载的AsyncTask + 回调接口

---
+++ app/src/main/java/com/zmj/viewpagertest/downloadprocess/DownloadService.java
@@ -0,0 +1,19 @@
+package com.zmj.viewpagertest.downloadprocess;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ *下载数据Service类
+ */
+public class DownloadService extends Service {
+
+    public DownloadService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
