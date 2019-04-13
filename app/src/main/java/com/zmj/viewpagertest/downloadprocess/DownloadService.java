2f9034a46bbd00130c7e9ef20b943a7efc81539d
ZmjDns <292570927@qq.com>
2019-04-13 22:49:04 +0800

    断点下载的AsyncTask + 回调接口

--- app/src/main/java/com/zmj/viewpagertest/downloadprocess/DownloadService.java
+++ app/src/main/java/com/zmj/viewpagertest/downloadprocess/DownloadService.java
@@ -1,11 +1,17 @@
 package com.zmj.viewpagertest.downloadprocess;
 
 import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Binder;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
 import android.widget.Toast;
+
+import com.zmj.viewpagertest.MainActivity;
+import com.zmj.viewpagertest.R;
 
 /**
  *下载数据Service类
@@ -59,11 +65,25 @@ public class DownloadService extends Service {
                 startForeground(1,getNotification("Downloading...",0));
                 Toast.makeText(DownloadService.this,"Downloading...",Toast.LENGTH_SHORT).show();
             }
+
         }
 
-        private Notification getNotification(String title,int progress){
-            return null;
-        }
 
+
+    }
+
+    private Notification getNotification(String title,int progress){
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pi = PendingIntent.getActivity(this,0,intent,0);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher));
+        builder.setContentIntent(pi);
+        builder.setContentTitle(title);
+        if (progress >= 0){
+            builder.setContentText(progress + "%");
+            builder.setProgress(100,progress,false);
+        }
+        return builder.build();
     }
 }
